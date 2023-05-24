808387ed4f942bad336c04407278c632946a31d1
flip701 <paulshipspb@gmail.com>
2023-05-24 12:58:26 +0300

    Решение первой задачи

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите M: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = m; i <= n; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.ReadLine();
+    }
+}
+
