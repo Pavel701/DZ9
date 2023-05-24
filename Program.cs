118190c6eef39dff5ef6ae24831f1b445846f1da
flip701 <paulshipspb@gmail.com>
2023-05-24 13:02:08 +0300

    Решение второй задачи

--- Program.cs
+++ Program.cs
@@ -1,4 +1,25 @@
-﻿class Program
+﻿// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.Write("Введите M: ");
+//         int m = int.Parse(Console.ReadLine());
+
+//         Console.Write("Введите N: ");
+//         int n = int.Parse(Console.ReadLine());
+
+//         for (int i = m; i <= n; i++)
+//         {
+//             Console.WriteLine(i);
+//         }
+
+//         Console.ReadLine();
+//     }
+// }
+
+//////////////////////////////////////////////////////////////////////////////
+
+class Program
 {
     static void Main(string[] args)
     {
@@ -8,12 +29,14 @@
         Console.Write("Введите N: ");
         int n = int.Parse(Console.ReadLine());
 
+        int sum = 0;
         for (int i = m; i <= n; i++)
         {
-            Console.WriteLine(i);
+            sum += i;
         }
+
+        Console.WriteLine($"Сумма натуральных элементов от {m} до {n} равна {sum}");
 
         Console.ReadLine();
     }
 }
-
